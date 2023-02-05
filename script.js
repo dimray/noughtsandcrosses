de984da3b17d0fbf3c4a4eb2d2ed1a0a96e89d40
Dan <50465889+dimray@users.noreply.github.com>
2023-02-05 15:19:26 +0000

    Add files via upload
    Add draw functionality

--- script.js
+++ script.js
@@ -3,13 +3,30 @@ let noughts = []
 let crosses = []
 let gameOver = false;
 
-const winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
-
 const playGame = document.querySelector(".grid-container");
 const playerTurn = document.querySelector("h2");
 const startGame = document.querySelector(".btn");
 
-playGame.addEventListener("click", selector);
+const winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
+const checkForWin = function () {
+  let winner;
+  let stopListener;
+  winningCombinations.forEach(combination => {
+    if (combination.every(element => crosses.includes(element))) {
+      winner = playerTurn.textContent = "X wins!";
+      stopListener = playGame.removeEventListener("click", selector);
+      gameOver = true;
+    }
+    if (combination.every(element => noughts.includes(element))) {
+      winner = playerTurn.textContent = "O wins!";
+      stopListener = playGame.removeEventListener("click", selector);
+      gameOver = true;
+    }
+  });
+  return { winner, stopListener };
+};
+
 
 startGame.addEventListener("click", function () {
   const gameBoard = document.querySelectorAll(".square");
@@ -22,35 +39,37 @@ startGame.addEventListener("click", function () {
   crosses = [];
   playerTurn.textContent = "X goes first";
   playGame.addEventListener("click", selector);
+  gameOver = false;
 });
 
 function selector(e) {
   e.target.classList.toggle("clicked");
-  if (squaresClicked.includes(+e.target.id) === false) {
+
+  if (!squaresClicked.includes(+e.target.id)) {
     squaresClicked.push(+e.target.id);
+
     if (squaresClicked.length % 2 == 0) {
       e.target.innerText = "O";
       noughts.push(+e.target.id);
       playerTurn.textContent = "X's turn";
-      gameOver = true;
     } else {
       e.target.innerText = "X";
       crosses.push(+e.target.id);
       playerTurn.textContent = "O's turn";
-      gameOver = true;
-    }
-    winningCombinations.forEach(combination => {
-      if (combination.every(element => crosses.includes(element))) {
-        playerTurn.textContent = "X wins!";
-        playGame.removeEventListener("click", selector);
-      }
-      if (combination.every(element => noughts.includes(element))) {
-        playerTurn.textContent = "O wins!";
-        playGame.removeEventListener("click", selector);
-      }
-    });
+    };
+
+    checkForWin();
+
+    console.log(squaresClicked.length);
+    console.log(gameOver);
+
+    if (squaresClicked.length === 9 && gameOver === false) {
+      playerTurn.textContent = "It's a draw!"
+    };
   };
 };
+
+playGame.addEventListener("click", selector);
 
 
 
